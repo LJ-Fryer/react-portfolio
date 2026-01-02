54a8b5ab128ac7a396eb6853bb0742472db5dcd7
LJ-Fryer <ljf_aus@hotmail.com>
2026-01-02 19:02:33 +1100

    Add hero image and scss variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "modern-normalize";
-
-import "./App.css";
+import "./App.scss";
+import trapezeImage from "./assets/trapeze.png";
 
 function App() {
   return (
     <>
       <h1>React Portfolio</h1>
+      <img src={trapezeImage} alt="Flying high on a trapeze" />
     </>
   );
 }
