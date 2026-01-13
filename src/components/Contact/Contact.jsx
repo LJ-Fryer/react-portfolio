c1dc4186b5ab1b29497f6c5a775347713381a289
LJ-Fryer <ljf_aus@hotmail.com>
2026-01-13 23:52:12 +1100

    Add media queries headings

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -8,16 +8,21 @@ const Contact = () => {
     <div className={classes.contact} id="contact">
       <H3>Get in touch</H3>
       <div className={classes.contact_box}>
-        <a href="https://github.com/LJ-Fryer" target="_blank">
-          <FontAwesomeIcon icon={faGithub} className={classes.icon} />
+        <a
+          href="https://github.com/LJ-Fryer"
+          target="_blank"
+          className={classes.icon}
+        >
+          <FontAwesomeIcon icon={faGithub} />
         </a>
         <a
           href="https://www.linkedin.com/in/linda-fryer-ba2294ba/"
           target="_blank"
+          className={classes.icon}
         >
-          <FontAwesomeIcon icon={faLinkedin} className={classes.icon} />
+          <FontAwesomeIcon icon={faLinkedin} />
         </a>
-        <a href="mailto:codingyogi7@gmail.com">
+        <a href="mailto:codingyogi7@gmail.com" className={classes.icon}>
           <P>codingyogi7@gmail.com</P>
         </a>
       </div>
